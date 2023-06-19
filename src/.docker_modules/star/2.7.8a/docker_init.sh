25f39f08ccad9f75d5321302b2c32e6f400be2dd
xgrand <xavier.grand@inserm.fr>
2023-06-19 14:07:47 +0200

    New STAR 2.7.8a Dockerfile to Charliecloud (procps)

--- src/.docker_modules/star/2.7.8a/docker_init.sh
+++ src/.docker_modules/star/2.7.8a/docker_init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker pull xgrand/star:2.7.8a
-# docker build src/.docker_modules/star/2.7.8a/ -t 'xgrand/star:2.7.8a'
-# docker push xgrand/star:2.7.8a
-docker buildx build --platform linux/amd64,linux/arm64 -t "xgrand/star:2.7.8a" --push src/.docker_modules/star/2.7.8a
+# docker pull xgrand/star:2.7.8a
+docker build src/.docker_modules/star/2.7.8a/ -t 'xgrand/star:2.7.8a'
+docker push xgrand/star:2.7.8a
+# docker buildx build --platform linux/amd64,linux/arm64 -t "xgrand/star:2.7.8a" --push src/.docker_modules/star/2.7.8a
