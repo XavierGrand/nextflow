3b04e9e144703f162e74e578ea366a70fcc01bdd
xgrand <xavier.grand@ens-lyon.fr>
2024-01-05 16:39:27 +0100

    Add src/.docker_modules/readthroughbincounts/0.1/Dockerfile & docker_init.sh

--- src/.docker_modules/readthroughbincounts/0.1/docker_init.sh
+++ src/.docker_modules/readthroughbincounts/0.1/docker_init.sh
@@ -3,3 +3,8 @@
 docker build src/.docker_modules/readthroughbincounts/0.1 -t 'lbmc/readthroughbincounts:0.1'
 # docker push lbmc/readthroughbincounts:0.1
 # docker buildx build --platform linux/amd64,linux/arm64 -t "lbmc/readthroughbincounts:0.1" --push src/.docker_modules/readthroughbincounts/0.1
+
+# docker pull xgrand/readthroughbincounts:0.1
+# docker build src/.docker_modules/readthroughbincounts/0.1 -t 'xgrand/readthroughbincounts:0.1'
+# docker push xgrand/readthroughbincounts:0.1
+# docker buildx build --platform linux/amd64,linux/arm64 -t "xgrand/readthroughbincounts:0.1" --push src/.docker_modules/readthroughbincounts/0.1
